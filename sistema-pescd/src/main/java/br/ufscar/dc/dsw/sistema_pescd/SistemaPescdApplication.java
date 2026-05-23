f965b391f9926051faa59784603119e9194b101a
Juliano <julianovitorino709@gmail.com>
2026-05-23 11:52:44 -0300

    Adicionando projeto sistema-pescd

---
+++ sistema-pescd/src/main/java/br/ufscar/dc/dsw/sistema_pescd/SistemaPescdApplication.java
@@ -0,0 +1,13 @@
+package br.ufscar.dc.dsw.sistema_pescd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaPescdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaPescdApplication.class, args);
+	}
+
+}
---
+++ sistema-pescd/src/test/java/br/ufscar/dc/dsw/sistema_pescd/SistemaPescdApplicationTests.java
@@ -0,0 +1,13 @@
+package br.ufscar.dc.dsw.sistema_pescd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaPescdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
